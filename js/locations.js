92476eb87ccca6d6223f3cfa1a23afe069e6967b
laertos <laertalickolli@gmail.com>
2018-03-12 18:07:24 -0500

    populate file with locations' details

---
+++ js/locations.js
@@ -0,0 +1,66 @@
+var locations = [
+{
+	name: "Small Cheval",
+	lat: 41.9141613802915,
+	lng: -87.68013926970849
+},
+
+{
+	name: "Big Star Taco",
+	lat: 41.9091585
+	lng: -87.67712519999999
+},
+
+{
+	name: "Violet Hour",
+	lat: 41.9090357,
+	lng: -87.67776069999999 
+},
+
+{
+	name: "Parlor Pizza", 
+	lat: 41.9035235,
+	lng: -87.6734467
+},
+
+{
+	name: "Emporium Arcade bar",
+	lat: 41.9064557,
+	lng: -87.671848
+},
+
+{
+	name: "The Den Theatre",
+	lat: 41.9061256,
+	lng: -87.67053820000001
+},
+
+{
+	name: "The Wormhole",
+	lat: 41.9083338
+	lng: -87.67466
+},
+{
+	name: "Myopic Books", 
+	lat: 41.9097746,
+	lng: -87.6769326
+},
+
+{
+	name: "5411 Empanadas",
+	lat: 41.9103042,
+	lng: -87.679283
+},
+
+{
+	name: "Sushi Taku",
+	lat: 41.9033716,
+	lng: -87.67511090000001
+},
+
+{
+	name: "Reckless Records",
+	lat: 41.9070939,
+	lng: -87.6717903
+},
+]
