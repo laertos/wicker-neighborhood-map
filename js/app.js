0b34f4950fbd8ec77583b77d2cd4892bb1c5caf9
laertos <laertalickolli@gmail.com>
2018-03-16 15:15:32 -0500

    implemented knockout

--- js/app.js
+++ js/app.js
@@ -1,17 +1,11 @@
-$("#toggle").click(function() {
-  $(this).toggleClass("on");
-  $("#locations").slideToggle();
-});
-
+//declare global vars
 var map;
 
-function ViewModel () {
+function ViewModel() {
 	var self = this;
-
 	this.markers = [];
-
+	//implementation of the map
 	this.initMap = function() {
-
 	// Create the map centered in desired location and zoom
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: {lat:41.908803, lng:-87.679598},
@@ -20,15 +14,15 @@ function ViewModel () {
 		mapTypeControl: false
 	});
 
-	this.largeInfowindow = new google.maps.InfoWindow();
+	this.infowindow = new google.maps.InfoWindow();
 	//create markers on initialize using locations.js
 	for (var i = 0; i <= locations.length; i++) {
 		this.position = locations[i].location;
-		this.name = locations[i].name;
+		this.title = locations[i].title;
 		this.marker = new google.maps.Marker({
 		map: map,
 		position: this.position,
-		name: this.name,
+		title: this.title,
 		animation: google.maps.Animation.DROP,
 		id: i
 		});
@@ -37,13 +31,32 @@ function ViewModel () {
 	this.markers.push(this.marker);
 	//create onClick even to open infoWindow for each marker
 	this.marker.addListener('click', function() {
-		populateInfoWindow(this, largeInfowindow);
+		populateInfoWindow(this, infowindow);
 	});	
 	} 
   };
   this.initMap();
+
+  this.searchedLocation = ko.observable('');
+  this.searchList = ko.computed(function() {
+  	var match = [];
+  	for (var i = 0; i <= this.markers.length; i++) {
+  		var locationsList = this.markers[i];
+  		if (locationsList.title.toLowerCase().includes(this.searchedLocation().toLowerCase())) {
+  			match.push(locationsList);
+  			this.markers[i].setVisible(true);
+  		} else {
+  			this.markers[i].setVisible(false);
+  		}
+  	}
+  	return match;
+  }, this);
 };
-ViewModel();
+
+
+function runMyApp() {
+    ko.applyBindings(new ViewModel());
+}
 
 
 
--- js/locations.js
+++ js/locations.js
@@ -1,66 +1,110 @@
 var locations = [
 {
-	name: "Small Cheval",
+	title: "Small Cheval",
 	location: {lat: 41.9141613802915, lng: -87.68013926970849},
 	category: "Burgers"
 },
 
 {
-	name: "Big Star Taco",
+	title: "Big Star Taco",
 	location: {lat: 41.9091585, lng: -87.67712519999999},
 	category: "Tacos"
 },
 
 {
-	name: "Violet Hour",
+	title: "Violet Hour",
 	location: {lat: 41.9090357, lng: -87.67776069999999},
 	category: "Cocktail Bar" 
 },
 
 {
-	name: "Parlor Pizza", 
+	title: "Parlor Pizza", 
 	location: {lat: 41.9035235, lng: -87.6734467},
 	category: "Pizza"
 },
 
 {
-	name: "Emporium Arcade bar",
+	title: "Emporium Arcade bar",
 	location: {lat: 41.9064557, lng: -87.671848},
 	category: "Bar"
 },
 
 {
-	name: "The Den Theatre",
+	title: "The Den Theatre",
 	location: {lat: 41.9061256, lng: -87.67053820000001},
 	category: "Entertainment"
 },
 
 {
-	name: "The Wormhole",
+	title: "The Wormhole",
 	location: {lat: 41.9083338, lng: -87.67466},
 	category: "Coffee Shop"
 },
 {
-	name: "Myopic Books", 
+	title: "Myopic Books", 
 	location: {lat: 41.9097746, lng: -87.6769326},
 	category: "Bookstore"
 },
 
 {
-	name: "5411 Empanadas",
+	title: "5411 Empanadas",
 	location: {lat: 41.9103042, lng: -87.679283},
 	category: "Argentinian Food"
 },
 
 {
-	name: "Sushi Taku",
+	title: "Sushi Taku",
 	location: {lat: 41.9033716, lng: -87.67511090000001},
 	category: "Sushi"
 },
 
 {
-	name: "Reckless Records",
+	title: "Reckless Records",
 	location: {lat: 41.9070939, lng: -87.6717903},
 	category: "Music Store"
 },
 ]
+
+
+/*
+
+ this.searchedLocation = ko.observable('');
+  this.searchList = ko.computed(function() {
+  	var match = [];
+  	for (var i = 0; i <= this.markers.length; i++) {
+  		var locationsList = this.markers[i];
+  		if (locationsList.title.toLowerCase().includes(this.searchedLocation.toLowerCase())) {
+  			match.push(locationsList);
+  			this.markers[i].setVisible(true);
+  		} else {
+  			this.markers[i].setVisible(false);
+  		}
+  	}
+  	return match;
+  }, this);
+
+  
+	var styles = [
+	{
+		featureType: 'water',
+		stylers: [
+		    {color: #19a0d8}
+		]
+	},{
+		featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [
+            { color: '#efe9e4' },
+            { lightness: -40 }
+        ]
+	},{
+		featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [
+            { visibility: 'on' },
+            { color: '#f0e4d3' }
+        ]
+	}	
+	];
+	https://sjobs.brassring.com/TGWebHost/jobdetails.aspx?SID=%5eKC1ScvlUoJ9GPhqzn0Bb2_slp_rhc_iDqG9A_slp_rhc_jdY4RRySEJgpoTukVf0GfEHCij0Tk%2fI12Pn&jobId=3391955&type=search&JobReqLang=1&recordstart=1&JobSiteId=5361&JobSiteInfo=3391955_5361&GQId=0
+*/
